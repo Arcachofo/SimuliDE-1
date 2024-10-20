fe2cb2da236cf07f6f6b3e78cbef83bf5881527a
arcachofo <arcachofo@simulide.com>
2024-10-20 16:38:12 +0200

    SSD1306: fix Page Mode reset value.

--- src/gui/circuitwidget/components/outputs/displays/ssd1306.cpp
+++ src/gui/circuitwidget/components/outputs/displays/ssd1306.cpp
@@ -102,7 +102,6 @@ void Ssd1306::initialize()
     m_continue = false;
     m_command  = false;
     m_data     = false;
-    m_addrMode = HORI_ADDR_MODE;
 
     clearDDRAM();
     reset() ;
@@ -143,6 +142,8 @@ void Ssd1306::reset()
     m_scroll   = false;
     m_scrollR  = false;
     m_scrollV  = false;
+
+    m_addrMode = PAGE_ADDR_MODE;
 }
 
 void Ssd1306::updateStep()
@@ -294,11 +295,11 @@ void Ssd1306::proccessCommand()
 
     if( m_rxReg < 0x10 ) // 00-0F 0-15 Set Lower Colum Start Address for Page Addresing mode
     {
-        m_addrX = (m_addrX & ~0xF) | (m_rxReg & 0xF);
+        if( m_addrMode == PAGE_ADDR_MODE ) m_addrX = (m_addrX & ~0xF) | (m_rxReg & 0xF);
     }
     else if( m_rxReg < 0x20 ) // 10-1F 16-31 Set Higher Colum Start Address for Page Addresing mode
     {
-        m_addrX = (m_addrX & 0xF) | ((m_rxReg & 0xF) << 4);
+        if( m_addrMode == PAGE_ADDR_MODE ) m_addrX = (m_addrX & 0xF) | ((m_rxReg & 0xF) << 4);
     }
     else if( m_rxReg == 0x20 ) m_readBytes = 1; // 20 32 Set Memory Addressing Mode
     else if( m_rxReg == 0x21 ) m_readBytes = 2; // 21 33 Set Column Address (Start-End)
@@ -340,7 +341,7 @@ void Ssd1306::proccessCommand()
 
     else if( (m_rxReg>=0xB0) && (m_rxReg<=0xB7) )   // B0-B7 176-183 Set Page Start Address for Page Addresing mode
     {
-        m_addrY = m_rxReg & 0x07; // 0b00000111
+        if( m_addrMode == PAGE_ADDR_MODE ) m_addrY = m_rxReg & 0x07; // 0b00000111
     }
     // C0-C8 192-200 Set COM Output Scan Direction
     else if( m_lastCommand == 0xC0 ) m_scanInv = false;
